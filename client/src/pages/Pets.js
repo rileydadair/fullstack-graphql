814b63edaedac9ab35539695c42666f1ce584d4b
Riley Adair <radair@hagerty.com>
2022-01-23 11:14:28 -0600

    commit course code-along

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,21 +1,87 @@
-import React, {useState} from 'react'
-import gql from 'graphql-tag'
-import { useQuery, useMutation } from '@apollo/react-hooks'
-import PetsList from '../components/PetsList'
-import NewPetModal from '../components/NewPetModal'
-import Loader from '../components/Loader'
+import React, { useState } from "react";
+import gql from "graphql-tag";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import PetsList from "../components/PetsList";
+import NewPetModal from "../components/NewPetModal";
+import Loader from "../components/Loader";
 
-
-export default function Pets () {
-  const [modal, setModal] = useState(false)
-
-
-  const onSubmit = input => {
-    setModal(false)
+const PETS_FIELDS = gql`
+  fragment PetsFields on Pet {
+    id
+    name
+    type
+    img
+    vaccinated @client #client-side directive
+    owner {
+      id
+      age @client
+    }
   }
-  
+`;
+
+const ALL_PETS = gql`
+  query AllPets {
+    pets {
+      ...PetsFields
+    }
+  }
+  ${PETS_FIELDS}
+`;
+
+const NEW_PET = gql`
+  mutation CreateAPet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      ...PetsFields
+    }
+  }
+  ${PETS_FIELDS}
+`;
+
+export default function Pets() {
+  const [modal, setModal] = useState(false);
+  const { data, loading, error } = useQuery(ALL_PETS);
+  const [createPet, newPet] = useMutation(NEW_PET, {
+    update(cache, { data: { addPet } }) {
+      const { pets } = cache.readQuery({ query: ALL_PETS });
+
+      cache.writeQuery({
+        query: ALL_PETS,
+        data: { pets: [addPet, ...pets] },
+      });
+    },
+  });
+
+  const onSubmit = (input) => {
+    setModal(false);
+    createPet({
+      variables: { newPet: input },
+      optimisticResponse: {
+        __typename: "Mutation",
+        addPet: {
+          __typename: "Pet",
+          id: Math.floor(Math.random() * 1000) + "",
+          name: input.name,
+          type: input.type,
+          img: "https://via.placeholder.com/300",
+          vaccinated: true,
+          owner: {
+            __typename: "User",
+            id: Math.floor(Math.random() * 1000) + "",
+            age: 0,
+          },
+        },
+      },
+    });
+  };
+
+  if (loading) return <Loader />;
+
+  if (error || newPet.error) return <p>ERROR</p>;
+
+  console.log(data.pets[0]);
+
   if (modal) {
-    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
+    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />;
   }
 
   return (
@@ -32,8 +98,8 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets} />
       </section>
     </div>
-  )
+  );
 }
